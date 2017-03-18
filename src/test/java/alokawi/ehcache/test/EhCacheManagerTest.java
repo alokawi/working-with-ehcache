aba4ec3e61f2ec1fa05971e3b7cf10882cb23bad
Alok Kumar <alokawi@gmail.com>
2017-03-18 21:39:46 +0530

    Add junit test case `testEhCachePutAndGet`

---
+++ src/test/java/alokawi/ehcache/test/EhCacheManagerTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package alokawi.ehcache.test;
+
+import java.util.UUID;
+
+import org.junit.Test;
+
+import alokawi.ehcache.core.EhCacheManager;
+import junit.framework.TestCase;
+
+/**
+ * @author alokkumar
+ *
+ */
+public class EhCacheManagerTest extends TestCase {
+
+	@Test
+	public void testEhCachePutAndGet() {
+		EhCacheManager cacheManager = new EhCacheManager();
+		for (int i = 0; i < 50000; i++) {
+
+			String key = UUID.randomUUID().toString();
+			String value = UUID.randomUUID().toString();
+			cacheManager.putCacheEntry(key, value);
+
+			assertEquals(value, cacheManager.getCacheEntry(key));
+
+		}
+	}
+
+}
